e4254a0b8dfc2a700bc63724a0cfc3a5a6304cc4
Khalid Mashal <Khalid.Mashal@geappliances.com>
2023-06-19 14:33:13 -0400

    change

--- dinov2/inference.py
+++ dinov2/inference.py
@@ -1,6 +1,6 @@
 from dinov2.models.vision_transformer import vit_base
 
-modle =vit_base()
+modle = vit_base()
 
-for name, param in modle.named_parameters:
+for name, param in modle.named_parameters():
     print(name, param.shape)
